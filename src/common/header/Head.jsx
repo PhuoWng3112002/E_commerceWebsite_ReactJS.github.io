da457757a77a904d7414d2a284d56b47d41cc349
phuong <thphg311@gmail.com>
2023-09-12 23:57:52 +0700

    Add new prj

---
+++ src/common/header/Head.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Head = () => {
+  return (
+    <>
+      <section className="head">
+        <div className="container d_flex">
+          <div className="left row">
+            <i className="fa fa-phone"></i>
+            <label> 0886127016</label>
+            <i className="fa fa-envelope"></i>
+            <label>thphg311@gmail.com</label>
+          </div>
+          <div className="right row RText">
+            <label>Theme FAQ"s</label>
+            <label>Need Help?</label>
+            <span>🏳️</span>
+            <label>EN</label>
+            <span>🏳️</span>
+            <label>USD</label>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Head;
